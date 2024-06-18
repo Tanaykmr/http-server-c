7f75c16cf3bfe9b343c04ce717ff4ed2f026e50d
Tanay Kumar <tanaykmrdev@gmail.com>
2024-06-18 17:21:10 +0530

    removed executable

--- webserver.c
+++ webserver.c
@@ -1,5 +1,14 @@
+#include <errno.h>
 #include <stdio.h>
-int main(){
-	printf("hello world\n");
+#include <sys/socket.h>
+int main()
+{
+	// creating a socket
+	int sockfd = socket(AF_INET6, SOCK_STREAM, 0); // sockfd stands for socket file descriptor
+	if(sockfd == -1){
+		perror("webserver(socket)");
+		return 1;
+	}
+	printf("socket created successfully\n");
 	return 0;
 }
